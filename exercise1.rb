aa51f5be864b1989e6adbd6289d9959cc1d9de19
aayushadeshara333 <adeshara.aayush@gmail.com>
2022-03-02 14:46:07 +0530

    implemeted using inbuilt methods

--- exercise1.rb
+++ exercise1.rb
@@ -1,11 +1,21 @@
 def findIndices(arr, result)
     arr.each_with_index do |x, i|
-        ((i+1)..arr.length).each do |y|
-            return [y - 1, i] if (arr[y - 1] + x) == result && (arr[y - 1] != x)
+        ((i+1)...arr.length).each do |y|
+            return [i, y] if (arr[y] + x) == result
         end
     end
     return "Not Found"
 end
 
-answer = findIndices([1, 2, 3, 4, 5, 99], 103)
+#Using inbuilt methods.
+def indices(arr, result)
+    combs = (0...arr.length).to_a.combination(2).to_a
+    obtained = combs.select {|f, s| arr[f] + arr[s] == result}.flatten
+    return "Not Found" if obtained.empty?
+    return obtained
+end
+
+answer = findIndices([1, 2, 3, 4, 5, 99], 104)
+p answer
+answer = indices([1, 2, 3, 4, 5, 99], 104)
 p answer
--- exercise3.rb
+++ exercise3.rb
@@ -12,5 +12,12 @@ def removeDuplicates(arr)
     end
     return check + [nil] * count
 end
+
+def remDup(arr)
+    distinct = arr.uniq
+    return distinct + [nil] * (arr.length - distinct.length)
+end
 answer = removeDuplicates([1, 2, 3, 1, 1, 1, 1, 2, 3, 4])
+p answer
+answer = remDup([1, 2, 3, 1, 1, 1, 1, 2, 3, 4])
 p answer
